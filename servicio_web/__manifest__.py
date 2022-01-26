b3814496c3371afa96c8d7fcf741f6c55e0ad874
Antonio Ruban <antoniodavid8@gmail.com>
2022-01-26 15:59:09 -0500

    [ADD] Enrolamiento

---
+++ servicio_web/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Servicios Web",
+
+    'summary': """
+       """,
+
+    'description': """
+           """,
+
+    'author': "Pavel",
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+
+    'depends': ['product'],
+
+    # always loaded
+    'data': [
+       'security/ir.model.access.csv',
+
+    ],
+	# only loaded in demonstration mode
+    'demo': [
+       
+    ],
+    'qweb': [],
+    'application': True,
+    
+}
