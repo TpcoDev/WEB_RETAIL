ef08ffd00ae3a963fc8cb8a937f0a5445a2b3173
Antonio Ruban <antoniodavid8@gmail.com>
2022-02-08 23:54:54 -0500

    [ADD] WS Activos Consulta

--- ws_activos_query/controllers/controllers.py
+++ ws_activos_query/controllers/controllers.py
@@ -1,81 +1,89 @@
 # -*- coding: utf-8 -*-
+
+from odoo.tools.translate import _
 from odoo import http
+from odoo.http import request
+from datetime import datetime
+from bs4 import BeautifulSoup
 import json
+import sys
+import uuid
+from odoo import http
+from odoo.http import request, Response
+import jsonschema
+from jsonschema import validate
+import json
+import yaml
+import requests, json
+import logging
+
+_logger = logging.getLogger(__name__)
+from datetime import timedelta, datetime, date
 
 
 class OdooController(http.Controller):
 
     @http.route('/tpco/odoo/activo/query', auth="public", type="json", method=['POST'], csrf=False)
     def activo_query(self, **post):
-        post = json.loads(http.request.httprequest.data)
-        product_tmpl = http.request.env['product.template']
-        tipo_prenda = http.request.env['tipo.prenda']
-        marca = http.request.env['marca']
-        tamanno = http.request.env['tamanno']
-        origen = http.request.env['origen']
-        color = http.request.env['color']
-        genero = http.request.env['genero']
 
-        obj_tipo_prenda = tipo_prenda.sudo().search([('name', '=', post['params']['detalleActivos'][0]['tipoPrenda'])])
-        if not obj_tipo_prenda:
-            tipo_prenda_nuevo = tipo_prenda.sudo().create({'name': post['params']['detalleActivos'][0]['tipoPrenda']})
-            tipo_prenda_id = tipo_prenda_nuevo.id
-        else:
-            tipo_prenda_id = obj_tipo_prenda.id
-
-        obj_marca = marca.sudo().search([('name', '=', post['params']['detalleActivos'][0]['marca'])])
-        if not obj_marca:
-            marca_nuevo = marca.sudo().create({'name': post['params']['detalleActivos'][0]['marca']})
-            marca_id = marca_nuevo.id
-        else:
-            marca_id = obj_marca.id
-
-        obj_tamanno = tamanno.sudo().search([('name', '=', post['params']['detalleActivos'][0]['tamaño'])])
-        if not obj_tamanno:
-            tamanno_nuevo = tamanno.sudo().create({'name': post['params']['detalleActivos'][0]['tamaño']})
-            tamanno_id = tamanno_nuevo.id
-        else:
-            tamanno_id = obj_tamanno.id
-
-        obj_origen = tipo_prenda.sudo().search([('name', '=', post['params']['detalleActivos'][0]['origen'])])
-        if not obj_origen:
-            origen_nuevo = origen.sudo().create({'name': post['params']['detalleActivos'][0]['origen']})
-            origen_id = origen_nuevo.id
-        else:
-            origen_id = obj_origen.id
-
-        obj_color = color.sudo().search([('name', '=', post['params']['detalleActivos'][0]['color'])])
-        if not obj_color:
-            color_nuevo = color.sudo().create({'name': post['params']['detalleActivos'][0]['color']})
-            color_id = color_nuevo.id
-        else:
-            color_id = obj_color.id
-
-        obj_genero = genero.sudo().search([('name', '=', post['params']['detalleActivos'][0]['genero'])])
-        if not obj_genero:
-            genero_nuevo = genero.sudo().create({'name': post['params']['detalleActivos'][0]['genero']})
-            genero_id = genero_nuevo.id
-        else:
-            genero_id = obj_genero.id
-
-        product_tmpl_nuevo = product_tmpl.sudo().create({
-            'name': post['params']['detalleActivos'][0]['nombreActivo'],
-            'tipo_prenda_id': tipo_prenda_id,
-            'marca_id': marca_id,
-            'tamanno_id': tamanno_id,
-            'origen_id': origen_id,
-            'color_id': color_id,
-            'genero_id': genero_id
-        })
-
-        return {
-            "idEnrolamiento": product_tmpl_nuevo.id,
-            "fechaOperacion": product_tmpl_nuevo.create_date,
-            "detalleActivos": [
-                {
-                    "EPCCode":  post['params']['detalleActivos'][0]['DetalleEpc'][0]['EPCCode'],
-                    "codigo": 0,
-                    "mensaje": "Activo enrolado"
-                }
-            ]
+        post = json.loads(request.httprequest.data)
+        res = {}
+        as_token = uuid.uuid4().hex
+        mensaje_error = {
+            "Token": as_token,
+            "RespCode": -1,
+            "RespMessage": "Error de conexión"
         }
+        mensaje_correcto = {
+            "Token": as_token,
+            "RespCode": 0,
+            "RespMessage": "OC recibidas correctamente"
+        }
+        try:
+            myapikey = request.httprequest.headers.get("Authorization")
+            if not myapikey:
+                mensaje_error['RespCode'] = -2
+                mensaje_error['RespMessage'] = f"Rechazado: API KEY no existe"
+                return mensaje_error
+
+            user_id = request.env["res.users.apikeys"]._check_credentials(scope="rpc", key=myapikey)
+            request.uid = user_id
+
+            if post['params']:
+                post = post['params']
+                vals = {}
+                lot = request.env['stock.production.lot'].sudo().search([('name', '=', post['EPCCode'])], limit=1)
+                location_id = None
+                location_parent_id = None
+                product_id = lot.product_id
+
+                for quant in lot.quant_ids:
+                    location_id = quant.location_id
+                    location_parent_id = quant.location_id.location_id
+
+                vals.update({
+                    'ubicacionPadre': location_parent_id.name,
+                    'ubicacion': location_id.name,
+                    'EPCCode': lot.name,
+                    'SKU': product_id.default_code,
+                    'nombreActivo': product_id.name,
+                    'tipoPrenda': product_id.tipo_prenda_id.name,
+                    'marca': product_id.marca_id.name,
+                    'tamaño': product_id.tamanno_id.name,
+                    'origen': product_id.origen_id.name,
+                    'color': product_id.color_id.name,
+                    'genero': product_id.genero_id.name,
+                    'codigo': 0,
+                    'mensaje': 'Activo existente en el inventario',
+
+                })
+                return vals
+
+        except Exception as e:
+            mensaje_error = {
+                "Token": as_token,
+                "RespCode": -5,
+                "RespMessage": "Rechazado: Autenticación fallida"
+            }
+            mensaje_error['RespMessage'] = f"Error: {str(e)}"
+            return mensaje_error
